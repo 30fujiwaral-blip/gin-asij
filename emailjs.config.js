eafd4b57b87f3bdf1a134197ba415bff3cc70756
30fujiwaral-blip <30fujiwaral@asij.ac.jp>
2025-12-23 15:19:53 +0900

    Add files via upload

---
+++ emailjs.config.js
@@ -0,0 +1,10 @@
+// Fill these with your EmailJS credentials
+// Get them from https://dashboard.emailjs.com
+// PUBLIC_KEY is safe to expose on the frontend.
+
+window.EMAILJS_CONFIG = {
+  PUBLIC_KEY: "C4yPxIW96I6mIZf-K",
+  SERVICE_ID: "service_tfiseac42525",
+  TEMPLATE_ID: "template_q4jaf1s",
+  TO_EMAIL: "lucafujiwara9@gmail.com"
+};
